d107af2d9d0760e5bd5f118b2f2f663efb062462
Stefan Hagen <stefan@hagen.link>
2022-07-24 16:34:49 +0200

    Created SMNVP app and minimal test to bootstrap the coverage chain

--- swid_lint/lint.py
+++ swid_lint/lint.py
@@ -1,2 +1,3 @@
-# -*- coding: utf-8 -*-
-# pylint: disable=expression-not-assigned,line-too-long
+def main():
+    """Later."""
+    return NotImplemented
--- test/test_lint.py
+++ test/test_lint.py
@@ -1,1 +1,5 @@
-# -*- coding: utf-8 -*-
+from swid_lint.lint import main
+
+
+def test_main():
+    assert main() is NotImplemented
