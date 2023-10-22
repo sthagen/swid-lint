6b0a308f6200a22caadea97c52cb106519bc63d9
Stefan Hagen <stefan@hagen.link>
2023-10-22 16:06:53 +0200

    Bumped impl version for release 2023.10.22

--- swid_lint/__init__.py
+++ swid_lint/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.10.22+parent.a2b07f84'
+# [[[end]]] (checksum: 6fbabfee32c9c3510a8969f35f34619c)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
