d8e2025f6700e4129c0f7d620e5fe8aec2ff5089
Stefan Hagen <stefan@hagen.link>
2023-10-22 15:54:06 +0200

    Silenced the type verifier to prepare for restructuring

--- swid_lint/cli.py
+++ swid_lint/cli.py
@@ -1,11 +1,11 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
 """Visit folder tree with SWID documents, validate the latter, and generate reports."""
 import sys
+from typing import no_type_check
 
 import swid_lint.lint as swid_lint
 
 
+@no_type_check
 def main(argv=None):
     """Process the job."""
     argv = sys.argv[1:] if argv is None else argv
--- swid_lint/lint.py
+++ swid_lint/lint.py
@@ -1,3 +1,7 @@
+from typing import no_type_check
+
+
+@no_type_check
 def main():
     """Later."""
     return NotImplemented
